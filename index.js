a95c1cdf6f2c45139f7a56197d2f373ee31c8e6a
Alexandre Gabriel <alexandregabrielc.silva@gmail.com>
2021-09-26 11:52:43 -0300

    Aula 34

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ let email = document.querySelector("#exampleInputEmail")
 let password =  document.querySelector("#exampleInputPassword")
 let photo =  document.querySelector("#exampleInputFile")
 let admin = document.querySelector("#exampleInputAdmin")
+
+let fields = document.querySelectorAll("#form-user-create [name]")
+
+fields.forEach(function(field, index){
+
+  if (field.name == 'gender') {
+    if (field.checked) console.log("Sim", field);
+  }else{
+    console.log("Não");
+  }
+
+  //console.log(field.checked);
+
+})
