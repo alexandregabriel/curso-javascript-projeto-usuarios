76a222fe36c79f1c26ee4ffb8c968d63b397a7df
Alexandre Gabriel <alexandregabrielc.silva@gmail.com>
2021-09-26 18:01:22 -0300

    Aula 37

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-let name = document.querySelector("#exampleInputName")
-let gender = document.querySelectorAll('#form-user-create [name=gender]:checked') //Selecionando os dois radios do do formuário usando o atributo name e o pseudo atributo checked
-let birth = document.querySelector("#exampleInputBirth")
-let country = document.querySelector("#exampleInputCountry")
-let email = document.querySelector("#exampleInputEmail")
-let password =  document.querySelector("#exampleInputPassword")
-let photo =  document.querySelector("#exampleInputFile")
-let admin = document.querySelector("#exampleInputAdmin")
+let fields = document.querySelectorAll('#form-user-create [name]')
+var user = {}
 
-let fields = document.querySelectorAll("#form-user-create [name]")
+function addLine(dataUser) {
+  let tr = document.createElement('tr')
 
-fields.forEach(function(field, index){
+  tr.innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+              <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+              <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+  `
 
-  if (field.name == 'gender') {
-    if (field.checked) console.log("Sim", field);
-  }else{
-    console.log("Não");
-  }
+  document.getElementById("table-users").appendChild(tr)
+}
 
-  //console.log(field.checked);
+document
+  .getElementById('form-user-create')
+  .addEventListener('submit', function (event) {
+    event.preventDefault()
 
-})
+    fields.forEach(function (field, index) {
+      if (field.name == 'gender') {
+        if (field.checked) {
+          user.gender = field.value
+        }
+      } else {
+        user[field.name] = field.value
+      }
+    })
+    addLine(user)
+  })
