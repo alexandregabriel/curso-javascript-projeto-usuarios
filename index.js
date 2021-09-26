83ac1cc98744410d5c7ffa6addaa85c9e37d09d6
Alexandre Gabriel <alexandregabrielc.silva@gmail.com>
2021-09-26 08:32:23 -0300

    Aula 32

---
+++ index.js
@@ -0,0 +1,8 @@
+let name = document.querySelector("#exampleInputName")
+let gender = document.querySelectorAll('#form-user-create [name=gender]:checked') //Selecionando os dois radios do do formuário usando o atributo name e o pseudo atributo checked
+let birth = document.querySelector("#exampleInputBirth")
+let country = document.querySelector("#exampleInputCountry")
+let email = document.querySelector("#exampleInputEmail")
+let password =  document.querySelector("#exampleInputPassword")
+let photo =  document.querySelector("#exampleInputFile")
+let admin = document.querySelector("#exampleInputAdmin")
