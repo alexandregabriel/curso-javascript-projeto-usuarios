ae7776a511efd1c6fc2ca2000a608af5156d884f
Alexandre Gabriel <alexandregabrielc.silva@gmail.com>
2021-10-05 12:57:50 -0300

    Aula 41

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,17 +9,43 @@ class UserController {
   onSubmit() {
     this.formEl.addEventListener('submit', event => {
       event.preventDefault()
-      this.addLine(this.getValues())
+      let values = this.getValues()
+
+      values.photo = '';
+
+      this.getPhoto((content) => {
+
+        values.photo = content
+
+        this.addLine(values)
+      });
+
     })
+  }
+
+  getPhoto(callback) {
+    let fileReader = new FileReader()
+    let elements = [...this.formEl.elements].filter(item=>{
+      if (item.name === 'photo') {
+        return item;
+      }
+    })
+
+    let file = elements[0].files[0];
+
+    fileReader.onload = () => {
+      callback(fileReader.result)
+    }
+
+    fileReader.readAsDataURL(file)
   }
 
   getValues() {
     let user = {}
-    
 
-    this.formEl.elements.forEach(function (field, index) {
+    ;[...this.formEl.elements].forEach(function (field, index) {
       if (field.name == 'gender') {
-        if (field.checked) { 
+        if (field.checked) {
           user[field.name] = field.value
         }
       } else {
@@ -43,7 +69,7 @@ class UserController {
     console.log(dataUser)
     this.tableEl.innerHTML = `
           <tr>
-              <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+              <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
               <td>${dataUser.name}</td>
               <td>${dataUser.email}</td>
               <td>${dataUser.admin}</td>
